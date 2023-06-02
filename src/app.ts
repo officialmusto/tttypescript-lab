e1eaa2e5d1e153aa6d1921ae47eb755922cd1102
Christian Musto <christianmustoj@gmail.com>
2023-06-02 04:16:25 -0400

    update variables

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,11 @@ turn: number,
 hasWinner: boolean, 
 hasTie: boolean
 
-
+const winningCombos: (number[])[] = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
+]
 
 // --------- CACHED ELEMENTS ---------
 
